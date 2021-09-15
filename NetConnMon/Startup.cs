ce8406ccb7d00442e42ad5769dcf5c5a7e5e4b14
Zach Baker <zachrybaker@gmail.com>
2021-09-15 01:01:19 -0500

    HSTS configurability

--- NetConnMon/Startup.cs
+++ NetConnMon/Startup.cs
@@ -48,6 +48,8 @@ namespace NetConnMon
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var useHsTs = Configuration.Get<OverridableSettings>()?.EnableHttpsUIRedirect ?? false;
+                
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -57,10 +59,11 @@ namespace NetConnMon
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                if(useHsTs)
+                    app.UseHsts();
             }
 
-            if (Configuration.Get<OverridableSettings>()?.EnableHttpsUIRedirect ?? false)
+            if (useHsTs)
                 app.UseHttpsRedirection();
             else
                 Console.WriteLine("Not enabling Https Redirection.");
